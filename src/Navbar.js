f9857474ba51a8c25a81072d980abfa8d7081206
Muhamad Suprianto <68896131+suprimen99@users.noreply.github.com>
2023-06-07 12:02:28 +0700

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+// // class component
+// class Navbar extends Component{
+//     render(){
+//         return (
+//           <div>
+//             <h3>
+//               test
+//             </h3>
+//           </div>
+//         )
+//     }
+//   }
+
+const Navbar = () => {
+    return(
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    )
+}
+
+// //   funtion component
+//  function Navbar ()  {
+//     return(
+//         <div>
+//             <h3>
+//                Navbar
+//            </h3>
+//         </div>
+//     )
+//  }
+
+  export default Navbar;
